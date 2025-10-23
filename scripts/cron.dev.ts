d463ac052430463d6656d338a344e2d2144cbff7
ThaungThanHan <ericlimmm115@gmail.com>
2025-10-23 09:10:17 +0700

    upate: cron

---
+++ scripts/cron.dev.ts
@@ -0,0 +1,13 @@
+// scripts/cron.ts
+import 'dotenv/config';
+
+import { cronManager } from '../src/lib/cron-manager';
+
+(async () => {
+  try {
+    console.log('[cron] started', new Date().toISOString());
+    await cronManager.initialize();
+  } catch (err) {
+    console.error('[cron] error', err);
+  }
+})();
